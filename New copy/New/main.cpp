08fc2cc6478093c9f54897797d872086efaf693a
moyert <55260804+moyert@users.noreply.github.com>
2019-11-26 10:16:28 -0500

    Update main.cpp

--- New copy/New/main.cpp
+++ New copy/New/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
 
-    cout << "\t\t    =====================================" << endl;
+    cout << "\t\t    ------------------------------------" << endl;
     cout << "\t\t    |---- ADD YOUR FAVORITE BEERS  ----|" << endl;
-    cout << "\t\t    =====================================" << endl << endl << endl;
+    cout << "\t\t    ------------------------------------" << endl;
 
     BeerList myBeerList;
     bool addBeer = true;
@@ -15,21 +15,21 @@ int main()
 
     while (addBeer == true)
     {
-        int option = 0;
-        cout << "===============================" << endl;
+        int answer = 0;
+        cout << "------------------------------" << endl;
         cout << "|---Please select an option---|" << endl;
-        cout << "|-------1 = Read File---------|" << endl;
-        cout << "|-------2 = Add Beer----------|" << endl;
-        cout << "|-------3 = Show List---------|" << endl;
-        cout << "|-------4 = Save--------------|" << endl;
-        cout << "|-------5 = Exit--------------|" << endl;
-        cout << "===============================" << endl;
-        cin >> option;
+        cout << "|------1 = Read Text File-----|" << endl;
+        cout << "|------2 = Add Beer-----------|" << endl;
+        cout << "|------3 = Show Temporary List|" << endl;
+        cout << "|------4 = Save---------------|" << endl;
+        cout << "|------5 = Exit---------------|" << endl;
+        cout << "-------------------------------" << endl;
+        cin >> answer;
         cin.clear();
         cin.get();
         
         //This is how the user can determine which option they would like to choose
-        switch (option)
+        switch (answer)
         {
             case 1:
                 myBeerList.readFromFile();
@@ -48,7 +48,7 @@ int main()
                 addBeer = false;
                 break;
             default:
-                cout << "invalid" << endl;
+                cout << "Invalid" << endl;
                 break;
         }
         
